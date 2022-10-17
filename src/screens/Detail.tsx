4ec083bf27a63912b6ff766222a58cbd73282c3b
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-10-17 09:04:30 -0300

    pokeapi react native

---
+++ src/screens/Detail.tsx
@@ -0,0 +1,81 @@
+import { useQuery } from "@tanstack/react-query";
+import { MainStackScreenProps } from "../navigators/types";
+import { fetchFn, fetchPokemon, Pokemon, Species } from "../utils/api";
+import {
+  AspectRatio,
+  Image,
+  Heading,
+  Stack,
+  HStack,
+  Center,
+} from "native-base";
+import { formatNumber, getTypeColor } from "../utils/helper";
+
+export function Detail({ route }: MainStackScreenProps<"Detail">) {
+  const { name } = route.params;
+  const { data } = useQuery<Pokemon>(["pokemon", name], () =>
+    fetchPokemon(name)
+  );
+  const { isLoading: isSpeciesLoading, data: species } = useQuery<Species>(
+    ["species", name],
+    () => fetchFn(data?.species.url || ""),
+    {
+      enabled: !!data,
+    }
+  );
+  if (!data) return null;
+  return (
+    <Stack>
+      <Center
+        safeArea
+        backgroundColor={getTypeColor(data.types[0].type.name) + ".500"}
+      >
+        <AspectRatio ratio={1} width="80%">
+          <Image
+            source={{
+              uri: data.sprites.other["official-artwork"].front_default,
+            }}
+            alt="image"
+          />
+        </AspectRatio>
+        <HStack
+          justifyContent="space-between"
+          width="100%"
+          p="3"
+          alignItems="center"
+          position="absolute"
+          bottom={0}
+          left={0}
+          right={0}
+        >
+          <Heading color="white" textTransform="capitalize" size="2xl">
+            {name}
+          </Heading>
+          <Heading color="white">#{formatNumber(data.id)}</Heading>
+        </HStack>
+      </Center>
+      <Stack p="3">
+        <HStack justifyContent="center">
+          {data.types.map((type) => (
+            <Center
+              key={type.type.name}
+              backgroundColor={getTypeColor(type.type.name) + ".500"}
+              rounded="full"
+              p="1"
+              minW="32"
+              _text={{
+                color: "white",
+                fontSize: "lg",
+                fontWeight: "bold",
+                textTransform: "capitalize",
+              }}
+              mx="2"
+            >
+              {type.type.name}
+            </Center>
+          ))}
+        </HStack>
+      </Stack>
+    </Stack>
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,40 @@
+import { PokemonCard } from "../components/PokemonCard";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { AllPokemon, fetchAllPokemon } from "../utils/api";
+import { Center, Spinner, FlatList } from "native-base";
+
+export function Home() {
+  const { data, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery<AllPokemon>(["pokemons"], fetchAllPokemon, {
+      getNextPageParam: (lastPage) => lastPage.next,
+    });
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  if (isLoading)
+    return (
+      <Center flex={1}>
+        <Spinner size="lg" color="white" />
+      </Center>
+    );
+  if (!data) return null;
+
+  return (
+    <FlatList
+      data={data.pages.flatMap((page) => page.results)}
+      keyExtractor={(item) => item.name}
+      renderItem={({ item }) => <PokemonCard name={item.name} />}
+      onEndReached={loadMore}
+      numColumns={2}
+      contentInsetAdjustmentBehavior="automatic"
+      ListFooterComponent={() =>
+        isFetchingNextPage ? <Spinner mt="4" size="lg" color="white" /> : null
+      }
+      _contentContainerStyle={{ p: 2, bg: "white" }}
+    />
+  );
+}
